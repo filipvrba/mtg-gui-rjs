14e0ece5240c9f0f62e40a0ec1a57de91b52d061
Filip Vrba <filipvrba@pm.me>
2023-02-09 10:32:32 +0100

    add elf-init

--- src/.js/elements/elm_cards.js
+++ src/.js/elements/elm_cards.js
@@ -1,9 +1,56 @@
 export default class ElmCards extends HTMLElement {
   constructor() {
-    super()
+    super();
+    this._h_init_elm = e => this.init_elm(e.detail.data)
   };
 
-  connectedCallback() {};
-  disconnectedCallback() {};
-  init_elm() {}
-}
+  connectedCallback() {
+    document.addEventListener(ElmCards.INIT, this._h_init_elm)
+  };
+
+  disconnectedCallback() {
+    document.removeEventListener(ElmCards.INIT, this._h_init_elm)
+  };
+
+  init_elm(data) {
+    let l_tbody = () => {
+      let result = "";
+
+      for (let i = 0; i < data.length; i++) {
+        let card = data[i];
+        let s_td = `${`
+        <tr>
+          <th scope='row' class='text-start'>${card.name}</th>
+          <th scope='row' class='text-center'>${card.types}</th>
+          <th scope='row' class='text-center'>${card.colorIdentity}</th>
+          <th scope='row' class='text-center'>${card.manaCost}</th>
+        </tr>
+        `}`;
+        result += `${s_td}\n`
+      };
+
+      return result
+    };
+
+    let template = `${`
+      <div class='table-responsive'>
+        <table class='table text-center'>
+          <thead>
+            <tr>
+              <th style='width: 31%;'></th>
+              <th style='width: 22%;'>Type</th>
+              <th style='width: 22%;'>Color</th>
+              <th style='width: 22%;'>Cast</th>
+            </tr>
+          </thead>
+          <tbody>
+            ${l_tbody.call()}
+          </tbody>
+        </table>
+      </div>
+    `}`;
+    this.innerHTML = template
+  }
+};
+
+ElmCards.INIT = "ech_init"
--- src/.js/main.js
+++ src/.js/main.js
@@ -2,9 +2,16 @@ import "../css/style.css";
 import "../css/bootstrap.min.css";
 import "./elements";
 import API from "./third-side/api";
+import ElmCards from "./elements/elm_cards";
 
 // document.querySelector('#app').innerHTML = "<h1>Hello RubyJS</h1>"
+function dispatch_data(data) {
+  let event = new CustomEvent(ElmCards.INIT, {detail: {data}});
+  document.dispatchEvent(event)
+};
+
+// Get Data
 API.get_result(
-  "SELECT * FROM cards DECS LIMIT 5",
-  data => console.log(data)
+  "SELECT * FROM cards WHERE hasFoil=0 LIMIT 5",
+  data => dispatch_data(data)
 )
